2ae6ed6962f12c5da5f407750b73033f7f40c1ea
DavPadur <164956758+DavPadur@users.noreply.github.com>
2024-06-29 11:43:46 -0700

    Update mac_install.sh

--- FlaskApp/mac_install.sh
+++ FlaskApp/mac_install.sh
@@ -1,16 +1,32 @@
-pip install --no-input flask 
-pip install --no-input selenium
-pip install --no-input -U pyvo
-curl https://storage.googleapis.com/chrome-for-testing-public/126.0.6478.55/mac-arm64/chromedriver-mac-arm64.zip 
+#!/bin/bash
 
-dir=$(pwd)
-echo -e "#!/bin/bash\npython $dir/app.py" >> macexec.command
-chmod u+x macexec.command
-cp macexec.command ~/Desktop
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
+fi
 
+# Install Python 3.9 using Homebrew
+if ! python3 --version | grep -q "3.9"
+then
+    echo "Installing Python 3.9..."
+    brew install python@3.9
+else
+    echo "Python 3.9 is already installed."
+fi
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Install dependencies
+python3 -m pip install --no-input flask selenium pyvo
+
+# Installation complete message
 echo
-echo -------------------------
-echo !!INSTALLATION COMPLETE!!
-echo -------------------------
+echo "-------------------------"
+echo "!!INSTALLATION COMPLETE!!"
+echo "-------------------------"
 echo
-
